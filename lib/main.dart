e66b7e6748424ea8e93ecc545daef6d3def462b4
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-12 22:22:32 +0100

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/onboard_screen_1.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
@@ -12,46 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const OnboardScreen1(),
+      home: const OnboardScreen1(),
     );
   }
 }
-
-
-
-
-// showModalBottomSheet(
-// isScrollControlled: true,
-// context: context,
-// builder: (context) {
-// return SingleChildScrollView(
-// child: Column(
-// mainAxisSize: MainAxisSize.min,
-// children: [
-// SvgPicture.asset("assets/Vector 9.svg"),
-// SizedBox(
-// height: 10,
-// ),
-// Row(
-// // mainAxisAlignment: MainAxisAlignment.center,
-//
-// children: [
-// Container(
-// margin: EdgeInsets.only(left: 120),
-// decoration: BoxDecoration(
-// color: Color(0xFF7047EB),
-// borderRadius: BorderRadius.circular(20,),),
-// height: 43,
-// width: 152,
-// child: const Row(
-// children: [
-// Padding(
-// padding: EdgeInsets.only(left: 28.0),
-// child: Text("Forecast report"),
-// ),
-// Icon(Icons.arrow_drop_down)
-// ],
-// ),
-// ),
-// ],
-//),
