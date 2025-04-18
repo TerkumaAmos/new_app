7ddee6bca1e3ed008d7a6175dab4b2dbc74971c6
TerkumaAmos <agberterkuma2020@gmail.com>
2025-04-19 00:32:35 +0100

    new

--- lib/learning_provider.dart
+++ lib/learning_provider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-class LearningProvider extends ChangeNotifier {}
+class LearningProvider extends ChangeNotifier {
+  int _count = 0;
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
+}
